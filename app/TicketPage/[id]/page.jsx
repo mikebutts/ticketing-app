d7fac483ea6d5885e74f628152074ecc27eb16ff
mikebutts <mgbutts@gmail.com>
2025-04-07 13:02:46 -0400

    updated dashboard to show filtered tickets

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,9 +1,11 @@
+import EditTicketForm from '@/app/(components)/TicketForm'
 import React from 'react'
 
 const TicketPage = ({params}) => {
   return (
     <div>
       <h1>TicketPage {params.id}</h1>
+      <EditTicketForm/>
     </div>
   )
 }
